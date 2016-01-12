697dfe21a11fce354ecce5d404510031429be41a
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-01-12 13:38:24 -0600

    skycon image appears on home view

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ionic-weather
+
+A weather app created using the Ionic framework
+
+Forecast.io used to gather weather info.
+
+Weather icons used from angular-skycons that interact with forecast.io well.
+
--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova', 'angular-skycons'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,71 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('root', {
+    url: '/root',
+    templateURL: 'templates/root.html'
+  })
+
+    // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('root');
+
+
+})
+
+//handles request to weather api
+.controller('weatherCtrl', function($scope, $q, $http, $cordovaGeolocation) {
+    $scope.temp;
+
+    var geoPosition = {};
+    $cordovaGeolocation
+     .getCurrentPosition(geoPosition)
+      .then(function (position) {
+
+     /// gets latitude  and longitude ///
+     var lat  = position.coords.latitude
+     var long = position.coords.longitude
+               
+     /// query the forcast api by latitude  and longitude ///        
+     $q(function(resolve, reject) {
+
+     /// gets current weather data with an ajax call ///
+     $http.get('/api/forecast/4368a97e3f99d4ca259858c9bbb8389f/'+lat+','+long)
+      .success(
+        function(weatherResponse) {
+        resolve(weatherResponse);
+
+     }, function(error) {  
+        console.log("there was an error");
+        reject(error);
+        }
+      );
+    }).then(function(weather){
+        console.log(" weather ", weather);
+
+      weather.temp = parseInt(weather.currently.temperature);
+
+      $scope.weatherImage = weather.currently.summary;
+
+      $scope.CurrentWeather = {
+        forecast: {
+          icon: "partly-cloudy-night",
+          iconSize: 150,
+          color: "black"
+        }
+      };
+    });
+
+      }, 
+       function(err) {
+         console.log("there was an error");
+      });
+
+});
